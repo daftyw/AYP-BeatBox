eb4f68a43f01c961686eca6f175073120ad6bdb2
dafty <rawin.n@augmentis.biz>
2016-08-08 15:35:14 +0700

    first draft 1

---
+++ app/src/main/java/ayp/aug/beatbox/BeatBoxActivity.java
@@ -0,0 +1,14 @@
+package ayp.aug.beatbox;
+
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BeatBoxActivity extends SingleFragmentActivity {
+
+
+    @Override
+    protected Fragment onCreateFragment() {
+        return BeatBoxFragment.newInstance();
+    }
+}
